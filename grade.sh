736c9b19b1c2388b39b2e003895f93b9ab382b58
enchin101 <122569330+enchin101@users.noreply.github.com>
2023-02-16 13:09:55 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -2,4 +2,11 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission
+if [[ -f ListExamples.java ]]
+then
+    echo "ListExamples file found"
+else
+    echo "need file ListExamples.java"
+fi
 echo 'Finished cloning'
